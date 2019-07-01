e8a9d9256b43b491e52c5375edcfc868825f583c
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-01 19:17:14 -0400

    addition of doubles and dispalying output in terminal

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -6,6 +6,10 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            double x = 36.78;
+            double y = 246.45;
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
 
             if (args.Length > 0) {
                 Console.WriteLine($"Hello, {args[0]}!, It's a pleasure to meet you.");
