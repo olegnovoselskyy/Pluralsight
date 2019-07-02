aeabd44a3e974157aba56f14530a5ac9ec644934
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-02 19:41:41 -0400

    Added constructor to grades

--- gradebook/src/Gradebook/Book.cs
+++ gradebook/src/Gradebook/Book.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 namespace GradeBook
 {
     class Book {
-        private List<double> list = new List<double>();
+        //private List<double> grades = new List<double>(); // Could also be written like this
+        private List<double> grades;
+
+        public Book() {
+            grades = new List<double>();
+        }
 
         public void AddGrade(double grade) {
 
-            list.Add(grade);
+            grades.Add(grade);
         }
 
         public int Count() {
-            return list.Count;
+            return grades.Count;
         }
 
     }
