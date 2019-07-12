cd2feee6beb72c6bf98591a78c563bdd91a0f520
Dmytro Lytvyn <dlytpi@gmail.com>
2019-07-12 17:56:30 -0400

    RD-1

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -7,6 +7,20 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void SetBookName()
+        {   
+           var book1 = GetBook("Book 1");
+           SetName(book1, "New Name");
+
+           Assert.Equal("New Name", book1.Name);     
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {   
            var book1 = GetBook("Book 1");
@@ -20,6 +34,7 @@ namespace GradeBook.Tests
            Assert.NotSame(book1, book2);         
         }
 
+        [Fact]
         public void TwoVarsCanReferenceSameObject()
         {   
            var book1 = GetBook("Book 1");
