b1dd52a3e41379db8db888bf471fc3153d7fee89
Oleg Novoselskyy <oleg.novoselskyy@gmail.com>
2019-07-13 09:31:30 -0400

    RD-1

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -6,8 +6,25 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
+
         [Fact]
-        public void Test1()
+        public void CsharpIsPassByValue()
+        {  
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+
+        [Fact]
+        public void CanSetNameFromReference()
         {  
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name");
